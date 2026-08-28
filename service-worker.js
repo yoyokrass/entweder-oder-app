bba90f828dbde8ac500d118d185715a2b8ca635a
yoyokrass <yoyojosu@hotmail.com>
2026-08-28 23:56:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,82 +1,99 @@
-const CACHE_NAME = "das-oder-das-v3";
+const CACHE_NAME = "das-oder-das-v4";
 
-const APP_FILES = [
+const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./style.css",
     "./app.js",
     "./manifest.json",
-    "./icons/icon-512.png"
+
+    "./design/background.png",
+    "./design/logo.png",
+    "./design/leiste.png",
+
+    "./icons/icon-512.png",
+    "./icons/apple-touch-icon.png"
 ];
 
 
-// Beim ersten Installieren:
-// App-Dateien lokal speichern
+/* =========================================
+   INSTALLIEREN
+   ========================================= */
+
 self.addEventListener("install", function (event) {
 
     event.waitUntil(
-
-        caches.open(CACHE_NAME).then(function (cache) {
-
-            return cache.addAll(APP_FILES);
-
-        })
-
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
     );
 
+    self.skipWaiting();
 });
 
 
-// Alten Cache entfernen,
-// falls wir später eine neue Version erstellen
+/* =========================================
+   AKTIVIEREN
+   ========================================= */
+
 self.addEventListener("activate", function (event) {
 
     event.waitUntil(
+        caches.keys()
+            .then(function (cacheNames) {
 
-        caches.keys().then(function (cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function (cacheName) {
 
-            return Promise.all(
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
 
-                cacheNames.map(function (cacheName) {
-
-                    if (cacheName !== CACHE_NAME) {
-
-                        return caches.delete(cacheName);
-
-                    }
-
-                })
-
-            );
-
-        })
-
+                    })
+                );
+            })
+            .then(function () {
+                return self.clients.claim();
+            })
     );
-
 });
 
 
-// Beim Laden zuerst Netzwerk versuchen.
-// Falls kein Internet vorhanden ist,
-// lokale Kopie verwenden.
+/* =========================================
+   DATEIEN LADEN
+   ========================================= */
+
 self.addEventListener("fetch", function (event) {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
 
     event.respondWith(
 
         fetch(event.request)
-
             .then(function (response) {
 
+                const responseCopy =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(function (cache) {
+
+                        cache.put(
+                            event.request,
+                            responseCopy
+                        );
+                    });
+
                 return response;
-
             })
-
             .catch(function () {
 
-                return caches.match(event.request);
-
+                return caches.match(
+                    event.request
+                );
             })
-
     );
-
 });
